6c6a7552a1dc353b216bebfc948e68521e686501
Park Jin soo <dndpr24@naver.com>
2022-07-09 23:49:43 +0900

    #13.8 With django forms, construct search form

--- rooms/forms.py
+++ rooms/forms.py
@@ -1,9 +1,25 @@
 from django import forms
+from django_countries.fields import CountryField
 from . import models
 
 
 class SearchForm(forms.Form):
 
+    country = CountryField(default="KR").formfield()
     city = forms.CharField()
     price = forms.IntegerField(required=False)
-    room_type = forms.ModelChoiceField(models.RoomType.objects.all())
+    room_type = forms.ModelChoiceField(
+        queryset=models.RoomType.objects.all(), required=False, empty_label="Any kind"
+    )
+    guests = forms.IntegerField(required=False)
+    besrooms = forms.IntegerField(required=False)
+    beds = forms.IntegerField(required=False)
+    baths = forms.IntegerField(required=False)
+    instant_book = forms.BooleanField(required=False)
+    superhost = forms.BooleanField(required=False)
+    amenities = forms.ModelMultipleChoiceField(
+        queryset=models.Amenity.objects.all(), widget=forms.CheckboxSelectMultiple
+    )
+    facilities = forms.ModelMultipleChoiceField(
+        queryset=models.Facility.objects.all(), widget=forms.CheckboxSelectMultiple
+    )
